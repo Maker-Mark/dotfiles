c4d905f9da2116454f7d30b81ee130ce6c21f0d0
EmNudge <24513691+EmNudge@users.noreply.github.com>
2022-09-19 19:25:21 -0400

    add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,19 +3,28 @@ alias ls="exa --icons -a"
 # tree view
 alias lt="exa --icons -a --tree"
 
-# better git graph
-alias gl="git log --all --graph --oneline"
+# force confirm
+alias rm='rm -i'
+
+# node quick aliases
+alias ni="npm ci"
+alias ns="npm start"
+alias nd="npm run dev"
+alias nb="npm run build"
+alias nt="npm run test"
 
 # force confirm
 alias rm='rm -i'
 
+# better git graph
+alias gl="git log --all --graph --oneline"
+alias gc="git checkout"
+
 # go to main branch, whether named master or main
-m() {
-    for branch in master main
-    do
-	if [ "git branch --list $branch" ]
-	then
-	    git checkout $(branch)
-	fi
-    done
+gcm() {
+	for branch in master main; do
+		if [ "`git branch --list $branch`" ]; then
+			git checkout $(branch)
+		fi
+	done
 }
